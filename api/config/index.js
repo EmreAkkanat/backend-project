bcf63af954d1c58c7e2d580e182084b91a8d4427
Emre-Akkanat <91632213+EmreAkkanat@users.noreply.github.com>
2023-08-17 17:37:57 +0300

    #6 user auth yapısı kuruldu

--- api/config/index.js
+++ api/config/index.js
@@ -6,4 +6,11 @@ module.exports = {
   CONNECTION_STRING:
     process.env.CONNECTION_STRING ||
     "mongodb://localhost:27017/project_base_ytb",
+
+  JWT: {
+    SECRET: "123456",
+    EXPIRE_TIME: !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME))
+      ? parseInt(process.env.TOKEN_EXPIRE_TIME)
+      : 24 * 60 * 60, // 86400
+  },
 };
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 var express = require("express");
 const bycript = require("bcrypt-nodejs");
 const is = require("is_js");
+const jwt = require("jwt-simple");
 
 const Users = require("../db/models/Users");
 const Response = require("../lib/Response");
@@ -8,6 +9,7 @@ const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
 const UserRoles = require("../db/models/UserRoles");
 const Roles = require("../db/models/Roles");
+const config = require("../config");
 var router = express.Router();
 
 /* GET users listing. */
@@ -264,6 +266,50 @@ router.post("/register", async (req, res) => {
     res
       .status(Enum.HTTP_CODES.CREATED)
       .json(Response.succesResponse({ succes: true }, Enum.HTTP_CODES.CREATED));
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+
+router.post("/auth", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    Users.validateFieldBeforeAuth(email, password);
+
+    let user = await Users.findOne({ email });
+
+    if (!user) {
+      throw new CustomError(
+        Enum.HTTP_CODES.UNAUTHORIZED,
+        "Validation Error",
+        "email or password wrong"
+      );
+    }
+
+    if (!user.validPassword(password)) {
+      throw new CustomError(
+        Enum.HTTP_CODES.UNAUTHORIZED,
+        "Validation Error",
+        "email or password wrong"
+      );
+    }
+
+    let payload = {
+      id: user._id,
+      exp: parseInt(Date.now() / 1000) * config.JWT.EXPIRE_TIME,
+    };
+
+    let token = jwt.encode(payload, config.JWT.SECRET);
+
+    let userData = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+    };
+
+    res.json(Response.succesResponse({ token, user: userData }));
   } catch (error) {
     let errorResponse = Response.errorResponse(error);
     res.status(errorResponse.code).json(errorResponse);
